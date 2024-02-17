5257048aaebb3386647a2f97fdb6f75d89186735
theanh <htheanh2000@gmail.com>
2024-02-17 21:21:06 +0700

    feat: init sign-in page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,9 +1,42 @@
-const SignInPage =() => {
-    return (
-        <div>
-            Signinpage
-        </div>
-    )
-}
+// pages/signin.tsx
+import type { NextPage } from 'next';
+import Button from '@/components/button';
+import Input from '@/components/input';
 
-export default SignInPage
+const SignInPage: NextPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white px-6">
+      <div className="w-full max-w-md">
+        <h2 className="text-2xl font-bold text-center mb-8">Login</h2>
+        <div className="space-y-4 mb-6">
+          <Input
+            label="email"
+            type="email"
+            placeholder="Your email address"
+          />
+          <Input
+          label="password"
+            type="password"
+            placeholder="Your password"
+          />
+        </div>
+
+        <Button className="w-full mb-3">
+          Login
+        </Button>
+
+        <div className="text-center">
+          <a href="#" className="text-sm text-primary hover:underline">
+            Forgot password?
+          </a>
+        </div>
+
+        <p className="text-xs text-center text-gray mt-6">
+          By continuing, you agree to the <strong>Terms of Services & Privacy Policy.</strong>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ src/components/input.tsx
@@ -0,0 +1,47 @@
+"use client"
+import { forwardRef, HTMLInputTypeAttribute, useImperativeHandle, useRef } from 'react'
+type Props = {
+  placeholder?: string
+  label?: string
+  className?: string
+  name?: string
+  onChange?:  any
+  type?: HTMLInputTypeAttribute
+  value?: string
+}
+
+export interface InputRef extends  HTMLInputElement  {
+  getValue: () => string
+}
+
+const Input = forwardRef((props: Props, ref, ) => {
+  const { placeholder, label, className, onChange, name = '', type , value } = props
+  const inputRef = useRef<HTMLInputElement>(null)
+  useImperativeHandle(ref, () => ({
+    getValue: () => inputRef?.current?.value,
+  }))
+
+  return (
+    <div className={`${className}`}>
+      <label htmlFor={label} className="lead capitalize">
+        {label}
+      </label>
+      <div className="mt-4">
+        <input
+          onChange={onChange}
+          type={type}
+          id={label}
+          name={name}
+          ref={inputRef}
+          value={value}
+          className={`w-full px-4 py-3 cursor-pointer base-text text-black outline rounded  outline-1 outline-gray focus:outline-primary`}
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
+  )
+})
+
+Input.displayName = 'Input'
+
+export default Input
